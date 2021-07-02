8189b48c6d98567a15b8530a0e29e3a7064a2466
张家喜 <zhangjiaxi@zhuofansoft.com>
2021-07-02 14:42:26 +0800

    rebuild

--- README.md
+++
@@ -1,5 +0,0 @@
-# 艾诺迪亚4新手交流群文档网站
-
-http://doc.inotia.cn
-
-本站使用 [Pagic](https://github.com/xcatliu/pagic) 构建，使用了 Pagic 默认的 blog 主题。
--- pagic.config.js
+++ pagic.config.js
@@ -2,7 +2,6 @@ export default {
     srcDir: 'src',
     theme: 'blog',
     plugins: ['blog'],
-    outDir: 'docs',
     title: '艾诺迪亚文档',
     description: '本文档由艾诺迪亚新手交流群成员提供与整理',
     github: 'https://github.com/fairymuzi/doc_inotia',
